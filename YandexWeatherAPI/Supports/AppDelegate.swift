53bc051bc5c9bc50aac22d69b2f0cdd317b9fcef
Denis Abramov <DenAnger@outlook.com>
2021-04-28 14:15:11 +0700

    refactoring

---
+++ YandexWeatherAPI/Supports/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  YandexWeatherAPI
+//
+//  Created by Denis Abramov on 28.04.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
